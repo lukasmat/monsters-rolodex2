6ad1cb02cc2b360c675269a1ae2a2991d0cd7581
Lukas <lukas147258@gmail.com>
2020-02-21 11:55:09 +0200

    Updates

---
+++ precache-manifest.b9d5d0cd3917498f1900ba1c32537b92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915828db9d49ce57f303172029a4085f",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "254cccfbb9b5b7166917",
+    "url": "/react-monsters-rolodex/static/css/main.d5c121bb.chunk.css"
+  },
+  {
+    "revision": "5561a3c434fb16450e04",
+    "url": "/react-monsters-rolodex/static/js/2.2b6e1509.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-monsters-rolodex/static/js/2.2b6e1509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "254cccfbb9b5b7166917",
+    "url": "/react-monsters-rolodex/static/js/main.cd23a087.chunk.js"
+  },
+  {
+    "revision": "683f4bc11af10aff1915",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.ec4a1e87.js"
+  }
+]);
